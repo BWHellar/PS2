811508082576a4dcf9647673630bf31c9e87485b
Brendan Hellar <bwhellar@Brendans-Air.hsd1.wa.comcast.net>
2019-06-01 18:58:51 -0700

    have all tests come back with success, and added comments

--- spec/Galactic-Age-Spec.js
+++ spec/Galactic-Age-Spec.js
@@ -2,21 +2,46 @@ import { GalacticAgeCalculator } from './../src/Galactic-Age.js';
 
 describe ('GalacticAgeCalculator', function()
 {
-  it('Should take in and process birthday', function()
+  it('Should take in and process birthday and return given birthday', function()
   {
     let newAge = new GalacticAgeCalculator('10-13-1989');
     let newDate = new Date('10-13-1989');
     expect(newAge.BirthDate()).toEqual(newDate);
   });
 
-  it('should return Mercury years', function()
+  it('should return known age rounded up according to birthday', function()
   {
-    let newCalculator = new GalacticAgeCalculator('10-013-1989');
+    let newCalculator = new GalacticAgeCalculator('10-13-1989');
+    expect(newCalculator.Age()).toEqual(30);
+  });
+
+  it('should return Mercury years rounded', function()
+  {
+    let newCalculator = new GalacticAgeCalculator('10-13-1989');
     expect(newCalculator.Mercury()).toEqual(7);
   });
-  it('should return Venus years', function()
+
+  it('should return Venus years rounded', function()
   {
-    let newCalculator = new GalacticAgeCalculator('10-013-1989');
+    let newCalculator = new GalacticAgeCalculator('10-13-1989');
     expect(newCalculator.Venus()).toEqual(19);
+  });
+
+  it('should return Mars years rounded', function()
+  {
+    let newCalculator = new GalacticAgeCalculator('10-13-1989');
+    expect(newCalculator.Mars()).toEqual(56);
+  });
+
+  it('should return Jupiter years rounded', function()
+  {
+    let newCalculator = new GalacticAgeCalculator('10-13-1989');
+    expect(newCalculator.Jupiter()).toEqual(356);
+  });
+
+  it('should return life left rounded', function()
+  {
+    let newCalculator = new GalacticAgeCalculator('10-13-1989');
+    expect(newCalculator.LifeLeft()).toEqual(-49);
   });
 });
--- src/Galactic-Age.js
+++ src/Galactic-Age.js
@@ -5,34 +5,42 @@ export class GalacticAgeCalculator
     this.birthDate = new Date(birthdate);
     this.currentDate = new Date();
   }
+  // Just your birthday
   BirthDate()
   {
     return this.birthDate;
   }
-
+  // This function will give the general age rounded to the nearest.
   Age()
   {
+    // Take the current date and subtract it by your birthdate
     let difference = this.currentDate - this.birthDate;
-    // This is the amount of Seconds in a year.
+    // Take the result above and divide it by the amount of seconds in the year.
     let age = difference / (31557600000);
+    // Return it to fixed.
     return parseFloat (age.toFixed());
   }
+  // We want to find your birthday on Mercury so we multiply by .24.
   Mercury()
   {
     return parseFloat ( (this.Age() * .24).toFixed() );
   }
+  // We want to find your birthday on Venus so we multiply by .62.
   Venus()
   {
     return parseFloat ( (this.Age() * .62).toFixed() );
   }
+  // We want to find your birthday on Mars so we multiply by 1.88.
   Mars()
   {
     return parseFloat ( (this.Age() * 1.88).toFixed() );
   }
+  // We want to find your birthday on Jupiter so we multiply by 11.86.
   Jupiter()
   {
     return parseFloat ( (this.Age() * 11.86).toFixed() );
   }
+  // We want to find how much life you have left to live so we subtract 79.
   LifeLeft()
   {
     return (this.Age().toFixed() - 79);
